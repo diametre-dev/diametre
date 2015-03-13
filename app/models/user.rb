e1c102d0c64a15e6d8930952578a6b8e03caa7d1
alice clavel <aliceclavel@wanadoo.fr>
2015-03-13 18:33:31 +0100

    wip account platform & dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable, :registerable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :posts
 end
