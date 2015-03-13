9109ddc45bd47bfd8cb650e5f4059db929b6c2e3
alice clavel <aliceclavel@wanadoo.fr>
2015-03-13 11:59:24 +0100

    set bucket and post model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312162507) do
+ActiveRecord::Schema.define(version: 20150313102053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,8 +35,12 @@ ActiveRecord::Schema.define(version: 20150312162507) do
     t.string   "title"
     t.text     "subtitle"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
