4ad67e0579e71ba601561d1f4a9728dac252b4fb
alice clavel <aliceclavel@wanadoo.fr>
2015-03-13 17:21:23 +0100

    front & form for posts & account controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'blog', to: "posts#index"
   get 'blog/:id', to: "posts#show"
+
+  resource :account, only: [:show, :edit, :update] do
+    resources :posts, module: :accounts
+  end
 end
