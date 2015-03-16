1fa75c66d20da9486b6e954c75e2a2d062bae35f
PavelCahn <david.cahn@thefamily.co>
2015-03-16 19:49:48 +0100

    blogpost

--- app/assets/javascripts/app/scrollContent.js
+++ app/assets/javascripts/app/scrollContent.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $(window).scroll(function() {
-   if($(document.body).scrollTop() > 700 && $('#about').hasClass('active')) {
+   if($(document.body).scrollTop() > 600 && $('#about').hasClass('active')) {
       $('#about').removeClass('active').addClass('inactive');
       $('#team').removeClass('inactive').addClass('active');
    }
-   if ($(document.body).scrollTop() < 700 && $('#team').hasClass('active')) {
+   if ($(document.body).scrollTop() < 600 && $('#team').hasClass('active')) {
       $('#team').removeClass('active').addClass('inactive');
       $('#about').removeClass('inactive').addClass('active');
    }
