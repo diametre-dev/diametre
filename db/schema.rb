31fccf07a639ec585fd64399625ac0ac4b98f4b0
PavelCahn <david.cahn@thefamily.co>
2015-03-20 09:47:29 +0100

    mvp galerie

--- db/schema.rb
+++ db/schema.rb
@@ -76,6 +76,12 @@ ActiveRecord::Schema.define(version: 20150319151812) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "suscribers", force: :cascade do |t|
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
