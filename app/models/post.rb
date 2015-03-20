9bad432c64f446e318e1d850694c6d147c56073d
alice clavel <aliceclavel@wanadoo.fr>
2015-03-20 15:32:22 +0100

    custom map view

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   has_attached_file :picture,
-    styles: { medium: "300x300>", thumb: "100x100>" }
+    styles: { medium: "400x400>", thumb: "100x100>" }
   belongs_to :user
 
   validates_attachment_content_type :picture,
